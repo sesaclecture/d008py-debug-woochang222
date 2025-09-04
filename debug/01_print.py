77dfdaffb864e5ce242c392a55e2fc6aed730e21
WooChangKim <nanenchanga2@gmail.com>
2025-09-04 10:56:47 +0900

    fix01

--- debug/01_print.py
+++ debug/01_print.py
@@ -1,7 +1,7 @@
 import random
 
 def find_max(numbers:list)->int:
-    max_num = 0
+    max_num = -51
     for num in numbers:
         if num > max_num:
             print(f"{max_num}이 새로운 값 {num} 보다 작아서 업데이트 합니다")
@@ -11,4 +11,5 @@ def find_max(numbers:list)->int:
 num_list = [random.randint(-50, 3) for _ in range(5)]
 print("Numbers: ", num_list)
 m = find_max(num_list)
+assert m == sorted(num_list, reverse=True)[0]
 print("Maximum value: ", m)
